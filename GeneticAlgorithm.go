15539db79d19ac54d35d0c4ee6052e8c96f1a9f8
Moulindra Muchumari <moulindra1729@gmail.com>
2017-03-02 22:19:43 -0500

    Added probability selection method

--- GeneticAlgorithm.go
+++ GeneticAlgorithm.go
@@ -28,7 +28,8 @@ func (g *GeneticAlgorithm) Run() {
 	g.eval()
 	sort.Sort(ByScore(g.pop))
 	fmt.Println(g.pop)
-
+	g.probSelection()
+	fmt.Println(g.pop)
 }
 
 /*
@@ -54,6 +55,10 @@ func newRand() {
 
 func randByte() byte {
 	return byte(r1.Intn(2))
+}
+
+func randFloat() float32 {
+	return r1.Float32()
 }
 
 func initByteArr(arr []byte) {
@@ -104,4 +109,41 @@ func (g *GeneticAlgorithm) eval() {
 		g.pop[sindex].score = count
 		index++
 	}
+}
+
+func (g *GeneticAlgorithm) getProbArr() ([]float32, []float32) {
+	total := 0
+	for _, speciman := range g.pop {
+		total += speciman.score
+	}
+
+	probArr := make([]float32, len(g.pop))
+	sumProbArr := make([]float32, len(g.pop))
+	for i, speciman := range g.pop {
+		probArr[i] = float32(speciman.score) / float32(total)
+		if i != 0 {
+			sumProbArr[i] = probArr[i] + sumProbArr[i-1]
+		} else {
+			sumProbArr[i] = probArr[i]
+		}
+	}
+	return probArr, sumProbArr
+}
+
+func (g *GeneticAlgorithm) probSelection() {
+	sort.Sort(ByScore(g.pop))
+
+	probArr, sumProbArr := g.getProbArr() // probabilites are ordered based on current order
+
+	tempPop := make([]Speciman, len(g.pop))
+	loopLen := len(g.pop) - 2
+	// the best individuals are put in the next generations
+	for i := 0; i < loopLen; i++ {
+		randNum := randFloat()
+		indexSelected := sort.Search(len(sumProbArr),
+			func(i int) bool { return sumProbArr[i] >= randNum })
+		tempPop[i] = g.pop[indexSelected]
+	}
+	tempPop[loopLen], tempPop[loopLen+1] = g.pop[loopLen], g.pop[loopLen+1]
+	g.pop = tempPop
 }
